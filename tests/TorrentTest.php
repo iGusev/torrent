9e19e8344f243480a19691ddc4f682195490e0ff
Илья Гусев <mail@igusev.ru>
2015-03-15 19:50:30 +0300

    phpunit tests

---
+++ tests/TorrentTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace League\Torrent\Test;
+
+use League\Torrent\Torrent;
+
+class TorrentTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Test that true does in fact equal true
+     */
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}

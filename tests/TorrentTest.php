fd4a10ca406fc60d286ebdd1a9af931b66391732
Илья Гусев <mail@igusev.ru>
2015-03-15 20:41:16 +0300

    test create torrent object from file

--- tests/TorrentTest.php
+++ tests/TorrentTest.php
@@ -7,6 +7,13 @@ use League\Torrent\Torrent;
 class TorrentTest extends \PHPUnit_Framework_TestCase
 {
 
+    public function testCreateFromFile()
+    {
+        $torrent = new Torrent('/Users/iGusev/torrent/tests/files/test2.torrent');
+        $torrent2 = Torrent::createFromFile('/Users/iGusev/torrent/tests/files/test2.torrent');
+
+        $this->assertEquals($torrent, $torrent2);
+    }
     /**
      * Test that true does in fact equal true
      */

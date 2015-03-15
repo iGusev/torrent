b8f6f2ae8e182866fb697cedbb3c1c0d7b04de7a
Илья Гусев <mail@igusev.ru>
2015-03-15 21:30:02 +0300

    add test for create from torrent url

--- tests/TorrentTest.php
+++ tests/TorrentTest.php
@@ -7,6 +7,13 @@ use League\Torrent\Torrent;
 class TorrentTest extends \PHPUnit_Framework_TestCase
 {
 
+    public function testCreateFromUrl()
+    {
+        $torrent = new Torrent('https://github.com/iGusev/torrent/raw/master/tests/files/test2.torrent');
+        $torrent2 = Torrent::createFromUrl('https://github.com/iGusev/torrent/raw/master/tests/files/test2.torrent');
+
+        $this->assertEquals($torrent, $torrent2);
+    }
     public function testCreateFromFile()
     {
         $torrent = new Torrent('/Users/iGusev/torrent/tests/files/test2.torrent');

60531dc473c74b492e7b03e522b22e2148f375c2
Илья Гусев <mail@igusev.ru>
2015-03-18 22:24:39 +0300

    clear test class

--- tests/TorrentTest.php
+++ tests/TorrentTest.php
@@ -2,30 +2,7 @@
 
 namespace League\Torrent\Test;
 
-use League\Torrent\Torrent;
-
 class TorrentTest extends \PHPUnit_Framework_TestCase
 {
 
-    public function testCreateFromUrl()
-    {
-        $torrent = new Torrent('https://github.com/iGusev/torrent/raw/master/tests/files/test2.torrent');
-        $torrent2 = Torrent::createFromUrl('https://github.com/iGusev/torrent/raw/master/tests/files/test2.torrent');
-
-        $this->assertEquals($torrent, $torrent2);
-    }
-    public function testCreateFromFile()
-    {
-        $torrent = new Torrent('/Users/iGusev/torrent/tests/files/test2.torrent');
-        $torrent2 = Torrent::createFromFile('/Users/iGusev/torrent/tests/files/test2.torrent');
-
-        $this->assertEquals($torrent, $torrent2);
-    }
-    /**
-     * Test that true does in fact equal true
-     */
-    public function testTrueIsTrue()
-    {
-        $this->assertTrue(true);
-    }
 }
